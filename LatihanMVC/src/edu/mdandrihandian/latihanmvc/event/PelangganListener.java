ab0d7bbb00a9df93467f9a12bcf25933880f08ec
MuhammadDandriHandian <55798941+MuhammadDandriHandian@users.noreply.github.com>
2020-01-08 01:05:18 +0700

    Add files via upload

---
+++ LatihanMVC/src/edu/mdandrihandian/latihanmvc/event/PelangganListener.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.mdandrihandian.latihanmvc.event;
+
+import edu.mdandrihandian.latihanmvc.model.PelangganModel;
+
+/**
+ *
+ * @author 
+ * NIM : 10118062
+ * NAMA : Muhammad Dandri Handian
+ * KELAS : IF-2
+ */
+public interface PelangganListener {
+    
+    public void onChange(PelangganModel pelanggan);
+    
+}
